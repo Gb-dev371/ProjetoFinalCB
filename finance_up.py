f41e81ca1f1fc48ce0be4cc20e7413744599ef45
Gb-dev371 <gbdev371@gmail.com>
2023-10-30 15:23:28 -0300

    pages folder added

--- finance_up.py
+++ finance_up.py
@@ -1,8 +1,49 @@
 import streamlit as st
 from utils.functions import *
+import sqlite3
 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+
+def login():
+    """ Simple Login App """
+    menu = ["Login", "SignUp"]
+    choice = st.sidebar.selectbox("", menu)
+
+    if choice == "Login":
+        st.sidebar.header("Sign in")
+        username = st.sidebar.text_input("User name")
+        password = st.sidebar.text_input("Password", type="password")
+
+        if st.sidebar.button("Login"): 
+            create_usertable(c)
+            result = login_user(c, username, password)
+            if result:
+                st.sidebar.success(f"Logged in as {username}.")
+            else:
+                st.sidebar.warning("Incorrect Username/Password")
+
+    elif choice == "SignUp":
+        st.sidebar.subheader("Create new account")
+        new_user = st.sidebar.text_input("Username")
+        new_password = st.sidebar.text_input("Password", type='password')
+
+        if st.sidebar.button("SignUp"):
+            create_usertable(c)
+            add_userdata(c, conn, new_user, new_password)
+            st.sidebar.success("You have successfully created a valid account. Now login with your account.")
+    
 
 def main():
+
+    st.set_page_config(
+    page_title="Finance Up",
+    page_icon="👋",
+    )
+
+    login()
+    
     st.title("FinanceUp - Gerenciamento Financeiro")
 
     renda_total = st.number_input("Informe sua renda total:", min_value=0.0)
