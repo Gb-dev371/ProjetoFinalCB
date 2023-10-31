57b2882c5c1b8a5089602f28120275e0b7eca110
Gb-dev371 <gbdev371@gmail.com>
2023-10-31 14:57:22 -0300

    update on functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import openpyxl as opx
 
 
 def create_dataframe(renda_total, gastos_essenciais, gastos_nao_essenciais, investimentos):
@@ -19,7 +18,7 @@ def create_dataframe(renda_total, gastos_essenciais, gastos_nao_essenciais, inve
 
 
 def create_grafico(renda_total, gastos_essenciais, gastos_nao_essenciais, investimentos):
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots()  # fig = figure, ax = axle
     df = create_dataframe(renda_total, gastos_essenciais, gastos_nao_essenciais, investimentos)
     ax.pie(df['Valor'], labels=df['Campo'], autopct='%1.1f%%')
     ax.set_title('Gráfico de Gastos')
